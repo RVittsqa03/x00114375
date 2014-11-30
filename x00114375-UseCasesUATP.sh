b54c421ea363729199b1280b21a7fb07acafa652
x00114375 <x00114375@gmail.com>
2014-11-30 23:00:20 +0000

    User Acceptance Test Plan

---
+++ x00114375-UseCasesUATP.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#User Acceptance Test Plan
+#Against seleniumhq.org website
+#Use Case #1 "edit this page"
+#Use Case #2 "contact"
+#Use Case #3 "2.8.0"
+
+
+echo '==================='
+echo 'Test Suite Summary:'
+echo '==================='
+echo 'Use Case #1' edit this page
+echo 'Use Case #2' contact
+echo 'Use Case #3' 2.8.0 
